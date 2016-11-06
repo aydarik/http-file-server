617837968c329985f1f7905682b16d8344b752c3
agumerbaev <aydar@gumerbaev.ru>
2016-11-06 22:54:51 +0300

    Конфигурация загрузки файлов.

--- src/main/java/httpfs/Application.java
+++ src/main/java/httpfs/Application.java
@@ -1,14 +1,32 @@
 package httpfs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@PropertySource("classpath:config.properties")
 public class Application {
+
+    @Value("${upload.maxSize}")
+    private String maxSize;
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(maxSize);
+        factory.setMaxRequestSize(maxSize);
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
